cc78cf9fa9d1f6d9fa63bd6d3b3d31a93310c9c3
Trey Terrell <trey.terrell@oregonstate.edu>
2014-08-29 14:22:23 -0700

    No double slashes

--- lib/heaven/notifier/irc.rb
+++ lib/heaven/notifier/irc.rb
@@ -4,7 +4,7 @@ module Heaven
       def deliver(msg)
         msg << " #{output_link('Output')}"
         Rails.logger.info "irc: #{msg}"
-        uri = URI.parse("#{message_url}/#{URI.escape(chat_room)}")
+        uri = URI.parse("#{message_url}/#{URI.escape(chat_room)}".gsub("//", "/"))
         http = Net::HTTP.new(uri.host, uri.port)
         request = Net::HTTP::Post.new(uri.request_uri)
         request.set_form_data({:payload => request_data(msg).to_json})
