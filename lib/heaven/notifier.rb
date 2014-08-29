4e2fbfaab3229f62f541ee32d8207d8275ba16d7
Trey Terrell <trey.terrell@oregonstate.edu>
2014-08-29 14:06:07 -0700

    Added IRC Notifier

--- lib/heaven/notifier.rb
+++ lib/heaven/notifier.rb
@@ -3,6 +3,7 @@ require 'heaven/notifier/campfire'
 require 'heaven/notifier/hipchat'
 require 'heaven/notifier/flowdock'
 require 'heaven/notifier/slack'
+require 'heaven/notifier/irc'
 
 module Heaven
   module Notifier
@@ -13,6 +14,8 @@ module Heaven
         ::Heaven::Notifier::Hipchat.new(payload)
       elsif flowdock?
         ::Heaven::Notifier::Flowdock.new(payload)
+      elsif irc?
+        ::Heaven::Notifier::Irc.new(payload)
       elsif Rails.env.test?
         # noop on posting
       else
@@ -22,6 +25,10 @@ module Heaven
 
     def self.slack?
       !!ENV['SLACK_TOKEN']
+    end
+
+    def self.irc?
+      !!ENV['HUBOT_IRC_MESSAGE_TOKEN']
     end
 
     def self.hipchat?
---
+++ lib/heaven/notifier/irc.rb
@@ -0,0 +1,30 @@
+module Heaven
+  module Notifier
+    class Irc < Notifier::Default
+      def deliver(msg)
+        msg << " #{output_link('Output')}"
+        Rails.logger.info "irc: #{message}"
+        uri = URI.parse("#{message_url}/#{URI.escape(chat_room)}")
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Post.new(uri.request_uri)
+        request.set_form_data({:payload => request_data(msg).to_json})
+        http.request(request)
+      end
+
+      def request_data(msg)
+        {
+          :token => message_token,
+          :msg => msg
+        }
+      end
+
+      def message_token
+        ENV['HUBOT_IRC_MESSAGE_TOKEN'] || ''
+      end
+
+      def message_url
+        ENV['HUBOT_IRC_MESSAGE_URL'] || ''
+      end
+    end
+  end
+end
